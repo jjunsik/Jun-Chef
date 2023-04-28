5503fcd8c6b04a5b43771e5178036d97422331bd
jjun <hjs8236@gmail.com>
2023-04-28 17:44:28 +0900

    Chore: MainActivity.java를 controller 패키지로 이동

--- app/src/main/java/main/java/controller/MainActivity.java
+++ app/src/main/java/main/java/controller/MainActivity.java
@@ -1,8 +1,10 @@
-package main.java;
+package main.java.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import main.java.R;
 
 public class MainActivity extends AppCompatActivity {
 
