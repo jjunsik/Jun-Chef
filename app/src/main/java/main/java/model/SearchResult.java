2867406263b4e5edebb1204f167c78c3db4272b8
jjun <hjs8236@gmail.com>
2023-05-03 14:10:36 +0900

    Feat: Response 값을 파싱한 모델 구현

--- app/src/main/java/main/java/model/SearchResult.java
+++ app/src/main/java/main/java/model/SearchResult.java
@@ -1,4 +1,25 @@
 package main.java.model;
 
 public class SearchResult {
+    private final String recipeName;
+    private final String[] ingredients;
+    private final String[] cookingOrder;
+
+    public SearchResult(String recipeName, String[] ingredients, String[] cookingOrder) {
+        this.recipeName = recipeName;
+        this.ingredients = ingredients;
+        this.cookingOrder = cookingOrder;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public String[] getCookingOrder() {
+        return cookingOrder;
+    }
 }
