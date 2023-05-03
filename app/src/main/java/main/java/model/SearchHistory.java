b885497f39f598d67f4736158c9f475067757a30
jjun <hjs8236@gmail.com>
2023-05-03 14:09:41 +0900

    Feat: 최근 기록에 대한 모델 구현

--- app/src/main/java/main/java/model/SearchHistory.java
+++ app/src/main/java/main/java/model/SearchHistory.java
@@ -1,4 +1,19 @@
 package main.java.model;
 
+import java.util.Date;
+
 public class SearchHistory {
+    private final String recipeName;
+    private final Date createDateTime;
+
+    public SearchHistory(String recipeName, Date createDateTime) {
+        this.recipeName = recipeName;
+        this.createDateTime = createDateTime;
+    }
+    public String getRecipeName() {
+        return recipeName;
+    }
+    public Date getCreateDateTime() {
+        return createDateTime;
+    }
 }
