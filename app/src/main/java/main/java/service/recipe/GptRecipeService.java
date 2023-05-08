caa28028586a2d19ab5a427a48d286873d983e0a
jjun <hjs8236@gmail.com>
2023-05-08 12:14:13 +0900

    Refactor: response 데이터를 받아서 검색 기록, 파서 클래스에 전달

--- app/src/main/java/main/java/service/recipe/GptRecipeService.java
+++ app/src/main/java/main/java/service/recipe/GptRecipeService.java
@@ -2,13 +2,12 @@ package main.java.service.recipe;
 
 import main.java.model.SearchHistory;
 import main.java.model.SearchResult;
-import main.java.service.util.HttpService;
-import main.java.service.util.ResultParser;
+import main.java.util.http.HttpService;
+import main.java.util.parser.ResultParser;
 import main.java.service.history.HistoryService;
 
 public class GptRecipeService extends RecipeService {
 
-    private static final String GPT_URL = "";
     public GptRecipeService(HttpService httpService, ResultParser resultParser, HistoryService historyService) {
         super(httpService, resultParser, historyService);
     }
@@ -16,17 +15,9 @@ public class GptRecipeService extends RecipeService {
     @Override
     protected void addHistory(SearchResult searchResult) {
         // searchResult 를 searchHistory 로 변경
-        SearchHistory searchHistory = null;
-
-
+        SearchHistory searchHistory = searchResult.toSearchHistory();
 
         // searchHistory 를 저장소에 추가
         historyService.addHistory(searchHistory);
-    }
-
-    @Override
-    protected String setURL() {
-        // url 정의
-        return GPT_URL;
     }
 }
