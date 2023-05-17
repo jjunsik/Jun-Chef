4dd5315ffed9ff3ddd04a0761c85ac5be7e08435
jjun <hjs8236@gmail.com>
2023-05-17 14:47:43 +0900

    Feat: 검색 기록을 로컬 저장소에 저장하기 위한 생성자

--- app/src/main/java/main/java/service/history/HistoryServiceImpl.java
+++ app/src/main/java/main/java/service/history/HistoryServiceImpl.java
@@ -3,8 +3,15 @@ package main.java.service.history;
 import java.util.List;
 
 import main.java.model.SearchHistory;
+import main.java.repository.HistoryRepository;
 
-public class HistoryServiceImpl implements HistoryService{
+public class HistoryServiceImpl implements HistoryService {
+    // 로컬 저장소에서 가져올 때 예외가 생긴다면 try-catch 문에서 예외 처리 해주는 클래스
+    private final HistoryRepository historyRepository;
+
+    public HistoryServiceImpl(HistoryRepository historyRepository) {
+        this.historyRepository = historyRepository;
+    }
 
     @Override
     public List<SearchHistory> getSearchHistories(int limit) {
