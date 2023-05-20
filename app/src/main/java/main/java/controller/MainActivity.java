bcd07c17caa52e6cb20bf3d4a65b9275e6cebd83
jjun <hjs8236@gmail.com>
2023-05-20 12:58:09 +0900

    Test: 최근 검색 기록의 로직 확인을 위한 테스트 코드

--- app/src/main/java/main/java/controller/MainActivity.java
+++ app/src/main/java/main/java/controller/MainActivity.java
@@ -1,10 +1,18 @@
 package main.java.controller;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.List;
 
 import main.java.R;
+import main.java.model.SearchHistory;
+import main.java.repository.HistoryRepository;
+import main.java.repository.LocalHistoryRepository;
+import main.java.service.history.HistoryService;
+import main.java.service.history.HistoryServiceImpl;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView1 = findViewById(R.id.item_test1);
+
+        HistoryRepository historyRepository = new LocalHistoryRepository(MainActivity.this);
+        HistoryService historyService = new HistoryServiceImpl(historyRepository);
+        historyService.addHistory(new SearchHistory("육회"));
+
+        List<SearchHistory> historyList = historyService.getSearchHistories(5);
+
+        textView1.setText(historyList.get(0).getRecipeName() + "---" + historyList.get(0).getCreateDateTime());
     }
 }
