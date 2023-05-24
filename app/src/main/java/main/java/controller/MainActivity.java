70784e50ce3f32ba391cdb82b9ef3258dd6a2c32
jjun <hjs8236@gmail.com>
2023-05-24 13:32:35 +0900

    Feat: 버튼 클릭 시 검색 화면으로 넘어가는 코드 작성

--- app/src/main/java/main/java/controller/MainActivity.java
+++ app/src/main/java/main/java/controller/MainActivity.java
@@ -1,18 +1,12 @@
 package main.java.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
-
 import main.java.R;
-import main.java.model.SearchHistory;
-import main.java.repository.HistoryRepository;
-import main.java.repository.LocalHistoryRepository;
-import main.java.service.history.HistoryService;
-import main.java.service.history.HistoryServiceImpl;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView1 = findViewById(R.id.item_test1);
-
-        HistoryRepository historyRepository = new LocalHistoryRepository(MainActivity.this);
-        HistoryService historyService = new HistoryServiceImpl(historyRepository);
-        historyService.addHistory(new SearchHistory("육회"));
-
-        List<SearchHistory> historyList = historyService.getSearchHistories(5);
-
-        textView1.setText(historyList.get(0).getRecipeName() + "---" + historyList.get(0).getCreateDateTime());
+        findViewById(R.id.startSearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
