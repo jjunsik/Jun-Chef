709ab0314d733e8f9b36beee2a9f31bc2b643f34
jjun <hjs8236@gmail.com>
2023-05-25 14:07:09 +0900

    Feat: 결과 화면 타이틀과 뒤로 가기 버튼 클릭 시 검색 화면으로 전환

--- app/src/main/java/main/java/controller/ResultActivity.java
+++ app/src/main/java/main/java/controller/ResultActivity.java
@@ -1,4 +1,33 @@
 package main.java.controller;
 
-public class ResultActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import main.java.R;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Toolbar searchActivityToolbar = findViewById(R.id.result_toolbar);
+        setSupportActionBar(searchActivityToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("검색 결과");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == android.R.id.home) {
+            Intent goToSearchActivity = new Intent(getApplicationContext(), SearchActivity.class);
+            startActivity(goToSearchActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
