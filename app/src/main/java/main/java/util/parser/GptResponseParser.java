f966ba394a7f892251276c82b7b5ba4f61b6c5bd
jjun <hjs8236@gmail.com>
2023-05-31 15:20:53 +0900

    Feat: API 통신을 통해 받은 데이터에서 실제로 필요한 text 부분까지 파싱

--- app/src/main/java/main/java/util/parser/GptResponseParser.java
+++ app/src/main/java/main/java/util/parser/GptResponseParser.java
@@ -1,10 +1,27 @@
 package main.java.util.parser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import main.java.model.SearchResult;
 
 public class GptResponseParser implements ResultParser{
     @Override
     public SearchResult getSearchResultByResponse(String response) {
+        // TODO: 파싱!
+
+        try {
+            JSONObject responseJSON = new JSONObject(response);
+            JSONArray responseArray = responseJSON.getJSONArray("choices");
+
+            JSONObject responseArrayJSON = responseArray.getJSONObject(0);
+            String responseText = responseArrayJSON.getString("text").trim();
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 }
