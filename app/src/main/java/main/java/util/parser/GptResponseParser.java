c94f86a4a2d8a706f4c09d106487b2cfac50a85d
jjun <hjs8236@gmail.com>
2023-06-09 14:52:40 +0900

    Feat: API 변경으로 인해 파싱 구조 변경. [재료], [레시피]에 대해 파싱하고 message라는 Dto를 만들어서 키가 content인 값 파싱

--- app/src/main/java/main/java/util/parser/GptResponseParser.java
+++ app/src/main/java/main/java/util/parser/GptResponseParser.java
@@ -18,13 +18,23 @@ public class GptResponseParser implements ResultParser{
         // 받은 response를 GptResponseDto 클래스 형식으로 파싱
             GptResponseDto responseDto = gson.fromJson(response, GptResponseDto.class);
 
-        // responseDto, choiceDto, splitText 등의 객체가 null인지 확인
-        if (responseDto != null && responseDto.getChoices() != null && !responseDto.getChoices().isEmpty()) {
-            GptResponseChoicesDto choiceDto = responseDto.getChoices().get(0);
+            // responseDto, choiceDto, splitText 등의 객체가 null인지 확인
+            if (responseDto != null && responseDto.getChoices() != null && !responseDto.getChoices().isEmpty()) {
 
-            if (choiceDto != null) {
-                String choiceText = choiceDto.getText().trim();
-                String[] splitText = choiceText.split("[재료]");
+                // responseDto에서 첫 번째 choice를 가져옴
+                GptResponseChoicesDto choiceDto = responseDto.getChoices().get(0);
+
+                if (choiceDto != null) {
+                    // choiceDto에서 message를 가져옴
+                    GptResponseMessageDto messageDto = choiceDto.getMessage();
+
+                    if (messageDto != null) {
+                        // messageDto에서 content 값을 가져옴
+                        String msgContent = messageDto.getContent().trim();
+
+                        // 정규식을 사용하여 [재료]와 [레시피]로 문자열을 분할
+                        String regex = "\\[(재료|레시피)\\]";
+                        String[] splitContent = msgContent.split(regex);
 
                 if (splitText.length > 1) {
                     ingredients.append("\n").append(splitText[1].split("[레시피]")[0].replace("\n\n", "\n").trim());
