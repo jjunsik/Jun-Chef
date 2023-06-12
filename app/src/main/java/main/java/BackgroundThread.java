a1af7e71b30319b37286c292cafb7ec8457271bd
jjun <hjs8236@gmail.com>
2023-06-12 13:06:29 +0900

    Feat: GPT 모델 변경으로 인한 API 통신 로직 변

--- app/src/main/java/main/java/BackgroundThread.java
+++ app/src/main/java/main/java/BackgroundThread.java
@@ -1,6 +1,12 @@
 package main.java;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import main.java.service.recipe.request.ChatGptRequest;
+import main.java.service.recipe.request.dto.GptRequestMessageDto;
 import main.java.util.http.HttpService;
 
 public class BackgroundThread implements Runnable {
@@ -13,6 +19,13 @@ public class BackgroundThread implements Runnable {
 
     @Override
     public void run() {
+
+        List<String> message = new ArrayList<>();
+        GptRequestMessageDto requestMessage = new GptRequestMessageDto(word);
+
+        Gson gson = new Gson();
+        message.add(gson.toJson(requestMessage));
+
         //http
         response = new HttpService().post(new ChatGptRequest(word + " 레시피"));
     }
