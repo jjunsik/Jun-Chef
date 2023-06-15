d1d81dfd2933410bfed83e324cae5afda013e1f3
jjun <hjs8236@gmail.com>
2023-06-15 16:35:56 +0900

    Feat: 검색어로 검색 후 다시 검색 화면으로 왔을 때, 검색어가 검색 창에 있었던 오류를 수정하기 위해 검색창 비우는 메서드 생성

--- app/src/main/java/main/java/controller/SearchActivity.java
+++ app/src/main/java/main/java/controller/SearchActivity.java
@@ -97,13 +97,57 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        // 뒤로 가기 버튼 클릭 시 호출
-        if (item.getItemId() == android.R.id.home) {
-            Intent goToMainActivity = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(goToMainActivity);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        super.onResume();
+        // 검색어 입력 상자 초기화
+        SearchView recipeSearch = findViewById(R.id.search_recipe);
+        recipeSearch.setQuery("", false);
+        recipeSearch.clearFocus();
     }
+
+//    private void updateSearchResults() {
+//        // 이전에 검색한 결과를 가져오는 로직
+//        // ...
+//        if (historyAdapter == null) {
+//            List<SearchHistory> searchHistories = historyService.getSearchHistories(5);
+//            if (searchHistories != null) { // null 체크
+//                historyAdapter = new HistoryRecyclerViewAdapter(searchHistories, SearchActivity.this);
+//                historyRecyclerView.setAdapter(historyAdapter);
+//            }
+//        }
+//
+//        // 검색 결과를 RecyclerView에 표시
+//        if (historyAdapter != null) {
+//            List<SearchHistory> searchHistories = historyService.getSearchHistories(5);
+//            if (searchHistories != null) {
+//                historyAdapter.setSearchHistories(searchHistories);
+//                historyAdapter.notifyDataSetChanged();
+//            }
+//        }
+//    }
+
+
+
+//    @Override
+////    툴바에 존재하는 뒤로 가기 버튼 뿐만 아니라 다른 버튼까지.
+////    즉, 툴바의 버튼을 제어하는 메서드
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        if (item.getItemId() == android.R.id.home) {
+//            Intent goToMainActivity = new Intent(SearchActivity.this, MainActivity.class);
+//            startActivity(goToMainActivity);
+//            finish();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//    @Override
+//    // 툴바 뿐만 아니라 모든 뒤로 가기 버튼에 대한 처리를 하는 메서드
+//    public void onBackPressed(){
+//        Intent goToMainActivity = new Intent(SearchActivity.this, MainActivity.class);
+//        startActivity(goToMainActivity);
+//        super.onBackPressed();
+//        // 뒤로 가기 버튼에 대한 동작을 처리할 때 액티비티를 종료하고 싶을 때 호출
+//        // 하지만, Intent를 사용해 다른 액티비티로 이동하는 코드에서는 일반적으로 사용 X
+//        // finish()를 해주지 않으면 첫 화면에서 뒤로 가기를 눌렀을 때, 앱이 종료되는 것이 아니라 다시 검색화면으로 나오므로 finish()를 해주어야 한다.
+//    }
 }
