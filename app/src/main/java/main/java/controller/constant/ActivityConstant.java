7e1094864f22a93b45c73c63e59adf9a4caa5eb3
jjun <hjs8236@gmail.com>
2023-06-19 21:29:59 +0900

    Feat: 뒤로 가기 두번 클릭 시 앱 종료하도록 하는 로직에서 변경 가능한 필드를 상수화 시킨 클래스

---
+++ app/src/main/java/main/java/controller/constant/ActivityConstant.java
@@ -0,0 +1,6 @@
+package main.java.controller.constant;
+
+public class ActivityConstant {
+    public static final int BACK_TIME = 1000;
+    public static final String BACK_TOAST_MESSAGE = "'뒤로' 버튼을 한번 더 누르시면 앱이 종료됩니다.";
+}
