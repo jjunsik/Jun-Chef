edc97917065ef82099dd89d545ae3e09621164ac
jjun <hjs8236@gmail.com>
2023-06-19 21:31:22 +0900

    Feat: 데이터에서 필요한 부분을 파싱했을 때, 그 필요한 부분에 대한 필드들을 상수화 시킨 클래스

---
+++ app/src/main/java/main/java/model/constant/ResultConstant.java
@@ -0,0 +1,7 @@
+package main.java.model.constant;
+
+public class ResultConstant {
+    public static final String RECIPE_NAME = "recipeName";
+    public static final String INGREDIENTS = "ingredients";
+    public static final String COOKING_ORDER = "cookingOrder";
+}
