3da39f06b6d0e35f065deea3d2934ca1920e1f05
jjun <hjs8236@gmail.com>
2023-06-20 12:57:53 +0900

    Feat: 앱의 첫 화면에서 뒤로 가기를 2번 클릭시 앱이 종료되도록 하는 로직

--- app/src/main/java/main/java/controller/MainActivity.java
+++ app/src/main/java/main/java/controller/MainActivity.java
@@ -1,14 +1,24 @@
 package main.java.controller;
 
+import static main.java.controller.constant.ActivityConstant.BACK_TIME;
+import static main.java.controller.constant.ActivityConstant.BACK_TOAST_MESSAGE;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import main.java.R;
+import main.java.controller.constant.ActivityConstant;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,24 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode != KeyEvent.KEYCODE_BACK)
+            return super.onKeyDown(keyCode, event);
+
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return true;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, BACK_TOAST_MESSAGE, Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(() ->
+                doubleBackToExitPressedOnce = false, BACK_TIME); // 1초 딜레이
+
+        return true;
     }
 }
