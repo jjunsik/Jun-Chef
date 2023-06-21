2a086dfb8c509672b8cdfde1deb01f74d0286a7f
jjun <hjs8236@gmail.com>
2023-06-21 13:36:50 +0900

    Refactor: API 호출 시 요청을 보낼 content 데이터의 고정값 상수화

--- app/src/main/java/main/java/service/recipe/request/dto/GptRequestMessageDto.java
+++ app/src/main/java/main/java/service/recipe/request/dto/GptRequestMessageDto.java
@@ -3,8 +3,9 @@ package main.java.service.recipe.request.dto;
 public class GptRequestMessageDto {
     String role = "user";
     String content;
+    public static final String FIXED_MESSAGE = " [재료]와 [레시피]";
 
     public GptRequestMessageDto(String content) {
-        this.content = content + " [재료]와 [레시피]";
+        this.content = content + FIXED_MESSAGE;
     }
 }
