1600e2bf886c21e30820fceb805bada88b86f203
jjun <hjs8236@gmail.com>
2023-06-21 13:34:42 +0900

    Feat: 결과 화면의 타이틀 텍스트 추가

--- app/src/main/java/main/java/controller/constant/ActivityConstant.java
+++ app/src/main/java/main/java/controller/constant/ActivityConstant.java
@@ -3,4 +3,5 @@ package main.java.controller.constant;
 public class ActivityConstant {
     public static final int BACK_TIME = 1000;
     public static final String BACK_TOAST_MESSAGE = "'뒤로' 버튼을 한번 더 누르시면 앱이 종료됩니다.";
+    public static final String SEARCH_RESULT_TITLE = "검색 결과";
 }
