d9fea01afd288d9093bdf11d5fee6b10fc2f61b6
jjun <hjs8236@gmail.com>
2023-06-21 13:40:07 +0900

    Refactor: 최근 검색 기록 아이템에서 날짜 형식을 상수화

--- app/src/main/java/main/java/model/SearchHistory.java
+++ app/src/main/java/main/java/model/SearchHistory.java
@@ -6,11 +6,12 @@ import java.time.format.DateTimeFormatter;
 public class SearchHistory {
     private final String recipeName;
     private final String createDateTime;
+    private static final String CREATED_DATE_TIME_FORMAT = "MM.dd";
 
     public SearchHistory(String recipeName) {
         this.recipeName = recipeName;
         LocalDate time = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM.dd");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(CREATED_DATE_TIME_FORMAT);
         this.createDateTime = time.format(formatter);
     }
     public String getRecipeName() {
