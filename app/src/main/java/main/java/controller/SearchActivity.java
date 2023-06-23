36e4493f713c9b8b6d33dbb7813152ecb2acc645
jjun <hjs8236@gmail.com>
2023-06-23 15:33:58 +0900

    Refactor: 리사이클러뷰 호출 로직이 필요하지 않으므로 삭제하고 로딩 다이얼로그의 종료시점을 추가하였다.

--- app/src/main/java/main/java/controller/SearchActivity.java
+++ app/src/main/java/main/java/controller/SearchActivity.java
@@ -6,7 +6,6 @@ import static main.java.model.constant.ResultConstant.RECIPE_NAME;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
@@ -17,7 +16,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import main.java.R;
 import main.java.adapter.HistoryRecyclerViewAdapter;
@@ -72,17 +70,15 @@ public class SearchActivity extends AppCompatActivity {
         historyAdapter = new HistoryRecyclerViewAdapter(searchHistories, SearchActivity.this);
         historyRecyclerView.setAdapter(historyAdapter);
 
-        recipeSearch.setOnQueryTextListener(getOnQueryTextListener(loadingDialog, historyRecyclerView));
+        recipeSearch.setOnQueryTextListener(getOnQueryTextListener(loadingDialog));
     }
 
-    private SearchView.OnQueryTextListener getOnQueryTextListener(LoadingDialog loadingDialog,
-                                                                  RecyclerView historyRecyclerView){
+    private SearchView.OnQueryTextListener getOnQueryTextListener(LoadingDialog loadingDialog){
         return new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 // 검색 버튼을 클릭 시 호출
                 loadingDialog.startLoadingDialog();
-                Toast.makeText(SearchActivity.this, "입력한 검색어: " + query, Toast.LENGTH_SHORT).show();
 
                 SearchResult result = recipeService.search(query);
 
@@ -98,11 +94,16 @@ public class SearchActivity extends AppCompatActivity {
                     historyRecyclerView.setAdapter(historyAdapter);
                 }
 
-                Intent goToResultActivity = new Intent(getApplicationContext(), ResultActivity.class);
-                goToResultActivity.putExtra(RECIPE_NAME, result.getRecipeName());
-                goToResultActivity.putExtra(INGREDIENTS, result.getIngredients());
-                goToResultActivity.putExtra(COOKING_ORDER, result.getCookingOrder());
-                startActivity(goToResultActivity);
+                    Intent goToResultActivity = new Intent(getApplicationContext(), ResultActivity.class);
+
+                    goToResultActivity.putExtra(RECIPE_NAME, result.getRecipeName());
+                    goToResultActivity.putExtra(INGREDIENTS, result.getIngredients());
+                    goToResultActivity.putExtra(COOKING_ORDER, result.getCookingOrder());
+
+                    loadingDialog.dismissDialog();
+
+                    startActivity(goToResultActivity);
+                });
 
                 return true;
             }
@@ -114,50 +115,4 @@ public class SearchActivity extends AppCompatActivity {
             }
         };
     }
-
-//    private void updateSearchResults() {
-//        // 이전에 검색한 결과를 가져오는 로직
-//        // ...
-//        if (historyAdapter == null) {
-//            List<SearchHistory> searchHistories = historyService.getSearchHistories(5);
-//            if (searchHistories != null) { // null 체크
-//                historyAdapter = new HistoryRecyclerViewAdapter(searchHistories, SearchActivity.this);
-//                historyRecyclerView.setAdapter(historyAdapter);
-//            }
-//        }
-//
-//        // 검색 결과를 RecyclerView에 표시
-//        if (historyAdapter != null) {
-//            List<SearchHistory> searchHistories = historyService.getSearchHistories(5);
-//            if (searchHistories != null) {
-//                historyAdapter.setSearchHistories(searchHistories);
-//                historyAdapter.notifyDataSetChanged();
-//            }
-//        }
-//    }
-
-
-
-//    @Override
-////    툴바에 존재하는 뒤로 가기 버튼 뿐만 아니라 다른 버튼까지.
-////    즉, 툴바의 버튼을 제어하는 메서드
-//    public boolean onOptionsItemSelected(MenuItem item){
-//        if (item.getItemId() == android.R.id.home) {
-//            Intent goToMainActivity = new Intent(SearchActivity.this, MainActivity.class);
-//            startActivity(goToMainActivity);
-//            finish();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-//    @Override
-//    // 툴바 뿐만 아니라 모든 뒤로 가기 버튼에 대한 처리를 하는 메서드
-//    public void onBackPressed(){
-//        Intent goToMainActivity = new Intent(SearchActivity.this, MainActivity.class);
-//        startActivity(goToMainActivity);
-//        super.onBackPressed();
-//        // 뒤로 가기 버튼에 대한 동작을 처리할 때 액티비티를 종료하고 싶을 때 호출
-//        // 하지만, Intent를 사용해 다른 액티비티로 이동하는 코드에서는 일반적으로 사용 X
-//        // finish()를 해주지 않으면 첫 화면에서 뒤로 가기를 눌렀을 때, 앱이 종료되는 것이 아니라 다시 검색화면으로 나오므로 finish()를 해주어야 한다.
-//    }
 }
