e8cd1d188b21d2f6708bb72fef37dc05a305bd04
jjun <hjs8236@gmail.com>
2023-06-23 15:15:52 +0900

    Feat: API 통신을 위한 로직에서 변경 가능한 필드를 추출하여 상수화 클래스를 생성

---
+++ app/src/main/java/main/java/util/http/constant/RequestConstant.java
@@ -0,0 +1,7 @@
+package main.java.util.http.constant;
+
+public class RequestConstant {
+    public static final String POST_METHOD_NAME = "POST";
+    public static final String GET_METHOD_NAME = "GET";
+    public static final String JSON_CONTENT_TYPE = "application/json";
+}
