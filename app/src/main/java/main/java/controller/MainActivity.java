a52cbbfb7ab1df3441594a8328a0419f6757150f
jjun <hjs8236@gmail.com>
2023-06-28 14:28:04 +0900

    Refactor: OnBackPressedCallback을 활용하여 뒤로 가기에 대한 로직 변경

--- app/src/main/java/main/java/controller/MainActivity.java
+++ app/src/main/java/main/java/controller/MainActivity.java
@@ -6,15 +6,14 @@ import static main.java.controller.constant.ActivityConstant.BACK_TOAST_MESSAGE;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.KeyEvent;
 import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import main.java.R;
 
 public class MainActivity extends AppCompatActivity {
-    
     private boolean doubleBackToExitPressedOnce = false;
 
     @Override
@@ -26,24 +25,21 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
             startActivity(intent);
         });
-    }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode != KeyEvent.KEYCODE_BACK)
-            return super.onKeyDown(keyCode, event);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (doubleBackToExitPressedOnce) {
+                    finishAffinity();
+                    System.exit(0);
+                } else {
+                    doubleBackToExitPressedOnce = true;
+                    Toast.makeText(MainActivity.this, BACK_TOAST_MESSAGE, Toast.LENGTH_SHORT).show();
 
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed();
-            return true;
-        }
-
-        this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, BACK_TOAST_MESSAGE, Toast.LENGTH_SHORT).show();
-
-        new Handler().postDelayed(() ->
-                doubleBackToExitPressedOnce = false, BACK_TIME); // 1초 딜레이
-
-        return true;
+                    new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, BACK_TIME);
+                }
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 }
