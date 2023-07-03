f4aa1874b716e1b9aff141b92c8e44a242c39208
jjun <hjs8236@gmail.com>
2023-07-03 15:34:54 +0900

    Feat: AlertDialog를 관리하는 클래스

---
+++ app/src/main/java/main/java/util/error/dialog/ErrorDialog.java
@@ -0,0 +1,45 @@
+package main.java.util.error.dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import main.java.util.error.ErrorFormat;
+
+public class ErrorDialog implements DialogInterface{
+
+    private final String title;
+    private final String message;
+
+    private static final String CHECK_BUTTON_TEXT = "확인";
+    Context context;
+    AlertDialog alertDialog;
+
+    public ErrorDialog(Context context, ErrorFormat errorFormat) {
+        this.context = context;
+        this.title = errorFormat.getTitle();
+        this.message = errorFormat.getMessage();
+    }
+
+    public void show(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton(CHECK_BUTTON_TEXT, (dialog, which) -> {
+            // 확인 버튼을 클릭했을 때 수행할 동작
+            dialog.dismiss(); // 다이얼로그 닫기
+        });
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void cancel() {
+        dismiss();
+    }
+
+    @Override
+    public void dismiss() {
+        alertDialog.dismiss();
+    }
+}
