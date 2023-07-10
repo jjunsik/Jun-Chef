ae73edd2f725bf5b6a7c8601a73e37f3b23e8bdd
jjun <hjs8236@gmail.com>
2023-07-10 16:22:33 +0900

    Refactor: Exception을 throw 하는 것 삭제

--- app/src/main/java/main/java/util/parser/ResultParser.java
+++ app/src/main/java/main/java/util/parser/ResultParser.java
@@ -1,8 +1,7 @@
 package main.java.util.parser;
 
 import main.java.model.SearchResult;
-import main.java.util.error.exception.SearchErrorException;
 
 public interface ResultParser {
-    SearchResult getSearchResultByResponse(String response) throws SearchErrorException;
+    SearchResult getSearchResultByResponse(String response);
 }
