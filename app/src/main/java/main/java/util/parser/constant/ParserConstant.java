4430c0dcce209da8bfd9b6a32bee5ac31697bb74
jjun <hjs8236@gmail.com>
2023-07-10 16:20:41 +0900

    Feat: 파싱 오류 처리에 대한 조건을 메서드로 추출

--- app/src/main/java/main/java/util/parser/constant/ParserConstant.java
+++ app/src/main/java/main/java/util/parser/constant/ParserConstant.java
@@ -5,4 +5,8 @@ public class ParserConstant {
     public static final String INGREDIENT_RECIPE_REGEX = "\\[(재료|레시피)]:|\\[(재료|레시피)]|(재료:|레시피:)";
     public static final String START_INGREDIENT_TEXT = "[재료]";
     public static final String START_COOKING_ORDER_TEXT = "[만드는 방법]";
+
+    public static boolean parserErrorCondition(StringBuilder ingredients, StringBuilder cookingOrder){
+        return ingredients.toString().length() < 10 || cookingOrder.toString().length() < 20;
+    }
 }
