9e70dd58b386b6e0f2cdd5b0fe8afa2134173f55
jjun <hjs8236@gmail.com>
2023-07-11 13:00:29 +0900

    Manifest, Adapter, Controller, Model 최종본 1

--- app/src/main/java/main/java/controller/ResultActivity.java
+++ app/src/main/java/main/java/controller/ResultActivity.java
@@ -20,7 +20,6 @@ import main.java.R;
 
 public class ResultActivity extends AppCompatActivity {
     TextView recipeNameTextView, ingredientsTextView, cookingOrderTextView;
-
     OnBackPressedCallback callback;
 
     @Override
@@ -30,6 +29,7 @@ public class ResultActivity extends AppCompatActivity {
 
         Toolbar searchActivityToolbar = findViewById(R.id.result_toolbar);
         setSupportActionBar(searchActivityToolbar);
+
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(SEARCH_RESULT_TITLE);
 
@@ -38,23 +38,16 @@ public class ResultActivity extends AppCompatActivity {
         setTextByExtra(INGREDIENTS, ingredientsTextView, R.id.result_ingredients);
         setTextByExtra(COOKING_ORDER, cookingOrderTextView, R.id.result_cookingorder);
 
-        // 생성자의 인자로 true를 전달하여 이 콜백이 기본 동작을 가로채도록 설정
-        // 이 콜백은 뒤로 가기 버튼 이벤트를 처리하는 역할
         callback = new OnBackPressedCallback(true) {
-            // 기기의 뒤로 가기 버튼 클릭에 대한 이벤트 처리하는 메서드
             @Override
             public void handleOnBackPressed() {
                 Intent goToSearchActivity = new Intent(ResultActivity.this, SearchActivity.class);
-                // 스택에 쌓이지 않게 하기 위한 코드
-                //  스택에 이미 존재하는 동일한 액티비티가 있을 경우 해당 액티비티 위의 모든 액티비티를 제거하고, 해당 액티비티가 최상위로 올라가도록 함
                 goToSearchActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(goToSearchActivity);
-                // 액티비티 종료를 위한 코드
                 finish();
             }
         };
 
-        // 생성한 callback 객체를 현재 액티비티의 OnBackPressedDispatcher에 등록하여 뒤로 가기 버튼 이벤트를 처리
         getOnBackPressedDispatcher().addCallback(this, callback);
     }
 
