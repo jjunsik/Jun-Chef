6f535c33c1f8ca06d1151c8d8319cda9022687e3
jjunsik <junsik8236@gmail.com>
2023-09-25 14:07:46 +0900

    Feat: 서버에 요청을 보낼 메서드 타입 delete와 put 추가

--- app/src/main/java/main/java/util/http/constant/RequestConstant.java
+++ app/src/main/java/main/java/util/http/constant/RequestConstant.java
@@ -2,8 +2,16 @@ package main.java.util.http.constant;
 
 public class RequestConstant {
     public static final String POST_METHOD_NAME = "POST";
+
     public static final String GET_METHOD_NAME = "GET";
+
+    public static final String DELETE_METHOD_NAME = "DELETE";
+
+    public static final String PUT_METHOD_NAME = "PUT";
+
     public static final String JSON_CONTENT_TYPE = "application/json";
+
     public static final int CONNECTION_TIME_OUT_VALUE = 3000;
+
     public static final int READ_TIME_OUT_VALUE = 60000;
 }
