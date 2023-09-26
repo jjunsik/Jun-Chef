4f21819ecdaee11ff50dcd7e5e51d30e9522e8db
jjunsik <junsik8236@gmail.com>
2023-09-26 19:58:11 +0900

    Feat: Member와 관련된 ResponseDto 구현

---
+++ app/src/main/java/main/java/util/parser/junchef/member/dto/GetMemberResponseDto.java
@@ -0,0 +1,25 @@
+package main.java.util.parser.junchef.member.dto;
+
+import java.util.List;
+
+import main.java.model.history.SearchHistory;
+
+public class GetMemberResponseDto {
+    private String name;
+
+    private String email;
+
+    private List<SearchHistory> histories;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<SearchHistory> getHistories() {
+        return histories;
+    }
+}
---
+++ app/src/main/java/main/java/util/parser/junchef/member/dto/MemberResponseDto.java
@@ -0,0 +1,9 @@
+package main.java.util.parser.junchef.member.dto;
+
+public class MemberResponseDto {
+    private Long memberId;
+
+    public Long getMemberId() {
+        return memberId;
+    }
+}
