048f7c8d6d6b6dcac9ea2c6460f2e9888afdea30
jjunsik <junsik8236@gmail.com>
2023-09-27 14:43:12 +0900

    Feat: Recipe 검색 및 조회의 ResponseDto 구현

---
+++ app/src/main/java/main/java/util/parser/junchef/recipe/dto/GetRecipeResponseDto.java
@@ -0,0 +1,21 @@
+package main.java.util.parser.junchef.recipe.dto;
+
+public class GetRecipeResponseDto {
+    private String recipeName;
+
+    private String ingredients;
+
+    private String cookingOrder;
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public String getCookingOrder() {
+        return cookingOrder;
+    }
+}
