a5e66bcb31bace2645aa2fd8c1b608c230e006e8
jjunsik <junsik8236@gmail.com>
2023-09-27 14:38:53 +0900

    Feat: JunChefRecipeService 구현

---
+++ app/src/main/java/main/java/service/junchef/recipe/JunChefRecipeService.java
@@ -0,0 +1,52 @@
+package main.java.service.junchef.recipe;
+
+import android.util.Log;
+
+import java.util.concurrent.CompletableFuture;
+
+import main.java.model.recipe.SearchResult;
+import main.java.util.error.junchef.JunChefException;
+import main.java.util.error.junchef.JunChefExceptionParser;
+import main.java.util.http.junchef.recipe.RecipeHttpService;
+import main.java.util.http.junchef.recipe.request.GetRecipeRequestDto;
+import main.java.util.parser.ResultParser;
+
+public class JunChefRecipeService {
+    private final RecipeHttpService recipeHttpService;
+    private final ResultParser resultParser;
+    private final JunChefExceptionParser exceptionParser = new JunChefExceptionParser();
+    private JunChefException junChefException;
+    SearchResult searchResult;
+
+    public JunChefRecipeService(RecipeHttpService recipeHttpService, ResultParser resultParser) {
+        this.recipeHttpService = recipeHttpService;
+        this.resultParser = resultParser;
+    }
+
+    public CompletableFuture<SearchResult> search(Long memberId, String word) {
+        return CompletableFuture.supplyAsync(() -> {
+            String response;
+            GetRecipeRequestDto getRecipeRequestDto = new GetRecipeRequestDto(memberId, word);
+
+            try {
+                response = recipeHttpService.getRecipeAndAddHistory(getRecipeRequestDto);
+
+                junChefException = exceptionParser.getJunChefException(response);
+
+                if (junChefException.getCode() != 0) {
+                    Log.d("junchef", "레시피 검색 및 조회 예외 받음" + junChefException.getCode() + junChefException.getTitle() + junChefException.getMessage());
+                    throw new JunChefException(junChefException.getCode(), junChefException.getTitle(), junChefException.getMessage());
+                }
+
+                searchResult = resultParser.getSearchResultByResponse(response);
+                searchResult.setRecipeName(word);
+
+                return searchResult;
+
+            } catch (JunChefException j) {
+                Log.d("junchef", "레시피 검색 및 조회 에러" + j.getCode() + j.getTitle() + j.getMessage());
+                throw j;
+            }
+        });
+    }
+}
