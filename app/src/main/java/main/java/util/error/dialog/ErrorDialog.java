2313e223c645e55e20d1abb6561796f0a2f388d8
jjunsik <junsik8236@gmail.com>
2023-10-02 13:37:51 +0900

    Feat: ErrorConstant 클래스를 활용하여 버튼의 텍스트 정의하는 부분 변경

--- app/src/main/java/main/java/util/error/dialog/ErrorDialog.java
+++ app/src/main/java/main/java/util/error/dialog/ErrorDialog.java
@@ -1,5 +1,7 @@
 package main.java.util.error.dialog;
 
+import static main.java.util.error.constant.ErrorConstant.CHECK_BUTTON_TEXT;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -7,13 +9,10 @@ import android.content.DialogInterface;
 import main.java.util.error.ErrorFormat;
 
 public class ErrorDialog implements DialogInterface{
-
     private final String title;
     private final String message;
-
-    private static final String CHECK_BUTTON_TEXT = "확인";
-    Context context;
-    AlertDialog alertDialog;
+    private final Context context;
+    private AlertDialog alertDialog;
 
     public ErrorDialog(Context context, ErrorFormat errorFormat) {
         this.context = context;
@@ -23,11 +22,15 @@ public class ErrorDialog implements DialogInterface{
 
     public void show(){
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
         builder.setTitle(title);
         builder.setMessage(message);
+
         builder.setPositiveButton(CHECK_BUTTON_TEXT, (dialog, which) -> {
-            dialog.dismiss();
+            // 확인 버튼을 클릭했을 때 수행할 동작
+            dialog.dismiss(); // 다이얼로그 닫기
         });
+
         alertDialog = builder.create();
         alertDialog.show();
     }
