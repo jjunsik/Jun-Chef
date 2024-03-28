04cd35efb3b69703a14f80843920416b27c2ddf0
jjunsik <junsik8236@gmail.com>
2024-03-28 23:21:48 +0900

    [Refactor] 서버에 요청하는 포트 번호를 8081 -> 8080 으로 변경

--- app/src/main/java/main/java/util/http/junchef/constant/JunChefBaseURL.java
+++ app/src/main/java/main/java/util/http/junchef/constant/JunChefBaseURL.java
@@ -8,7 +8,7 @@ public class JunChefBaseURL {
 
     // AVD는 나의 컴퓨터와 격리되어 가상 라우터와 방화벽 서비스 뒤에서 실행된다.
     // 따라서 내 컴퓨터의 로컬 ip 주소인 127.0.0.1은 AVD에서 10.0.2.2로 사용해야 한다.
-//    public static final String JUN_CHEF_MEMBER_BASE_URL = "http://10.0.2.2:8081/jun-chef/v1/members";
-//    public static final String JUN_CHEF_RECIPE_BASE_URL = "http://10.0.2.2:8081/jun-chef/v1/recipe";
-//    public static final String JUN_CHEF_HISTORY_BASE_URL = "http://10.0.2.2:8081/jun-chef/v1/history";
+    public static final String JUN_CHEF_MEMBER_BASE_URL = "http://10.0.2.2:8080/jun-chef/v1/members";
+    public static final String JUN_CHEF_RECIPE_BASE_URL = "http://10.0.2.2:8080/jun-chef/v1/recipe";
+    public static final String JUN_CHEF_HISTORY_BASE_URL = "http://10.0.2.2:8080/jun-chef/v1/history";
 }
