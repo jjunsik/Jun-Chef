4265287e6b2f6f36b07d4920d6d0b3807716a272
jjunsik <junsik8236@gmail.com>
2024-03-28 23:23:31 +0900

    [Feat] 세션이 없는 미인증 사용자에 대한 에러 Content 추가

--- app/src/main/java/main/java/util/error/junchef/JunChefExceptionContent.java
+++ app/src/main/java/main/java/util/error/junchef/JunChefExceptionContent.java
@@ -8,7 +8,8 @@ public enum JunChefExceptionContent {
     DUPLICATE_MEMBER_ERROR(5000, "중복 회원 오류", "중복된 이메일입니다.\n다른 이메일로 회원 가입해 주세요."),
     NON_EXIST_MEMBER_ERROR(5100, "회원 오류", "존재하지 않는 회원입니다.\n회원 가입 후 이용해 주세요."),
     NON_EXIST_MEMBER_EMAIL_ERROR(5200, "이메일 오류", "존재하지 않는 이메일입니다.\n이메일을 확인해 주세요."),
-    MEMBER_PASSWORD_ERROR(5300, "비밀번호 오류", "비밀번호가 일치하지 않습니다.\n비밀번호 확인해 주세요.");
+    MEMBER_PASSWORD_ERROR(5300, "비밀번호 오류", "비밀번호가 일치하지 않습니다.\n비밀번호를 확인해 주세요."),
+    UNAUTHENTICATED_MEMBER(4444, "미인증 사용자 요청 오류", "로그인 후 다시 시도해주세요.");
 
     private final int code;
     private final String title;
